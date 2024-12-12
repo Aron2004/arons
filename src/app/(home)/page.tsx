2117a1d8635673baa0eb0dbd7807857ff06c8e66
AronS <szijarto.aron1@gmail.com>
2024-12-12 20:28:44 +0100

    new

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,17 @@
-// src/app/(home)/page.tsx
+// src/app/page.tsx
 
 
-import Typography from "@mui/material/Typography";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/authOptions";
+import AuthHomeView from "../../sections/AuthHomeView";
+import NonAuthHomeView from "../../sections/NonAuthHomeView";
 
-export const metadata = {title: "Domov | ZoskaSnap"};
+export const metadata = { title: "Domov | ZoškaSnap" };
 
-export default function Home(){
+export default async function HomePage() {
+  // Fetch session on the server
+  const session = await getServerSession(authOptions);
 
-    return(
-
-        <Typography> Domovska Stranka </Typography>
-
-    );
+  // Conditionally render authenticated or non-authenticated home view
+  return session ? <AuthHomeView session={session} /> : <NonAuthHomeView />;
 }
